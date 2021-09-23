91535227047be9a995170feee1e302bff36be00f
Luiz Sanches <lsanches@protonmail.com>
2021-09-23 14:46:48 -0300

    Visualizar filas em segundo plano

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
   mount Decidim::Core::Engine => '/'
+
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
